7352b6d309b1773dd3f9893f959ceb440eba6693
Anand, Harsh <harsh.anand07@yahoo.com>
2018-09-12 10:59:34 +0530

    docker war deployment demo

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -1,27 +1,25 @@
 package com.springboot.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
-public class DemoApplication extends SpringBootServletInitializer  {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.sources(DemoApplication.class);
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	@RequestMapping(value = "/")
-	public String hello() {
-		return "Hello World";
-	}
+public class DemoApplication  extends SpringBootServletInitializer{
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(DemoApplication.class);
+   }
+   public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+   }
+   
+   @RequestMapping(value = "/")
+   public String hello() {
+      return "Hello World from Tomcat";
+   }
 }
